0bd3976b0b102a81694694b9a1db2719495ce564
SteffanBurnette <burnettesteffansep7@gmail.com>
2026-06-17 18:26:46 -0400

    form stuff

--- myStackedModal.js
+++ myStackedModal.js
@@ -3,6 +3,20 @@ const closeModal = document.getElementById("closeModal");
 const modal = document.getElementById("modal");
 const submitBtn = document.getElementById("submit_form");
 
+//form variables
+const username = document.getElementById("username");
+const email = document.getElementById("email");
+const usrNumber = document.getElementById("phone_number");
+
+const address = document.getElementById("address");
+const zipCode = document.getElementById("zip_code");
+const city = document.getElementById("city");
+const state = document.getElementById("state");
+
+//radio element selected value
+const gender = document.querySelector('input[name="gender"]:checked');
+const age = document.querySelector('input[name="age"]:checked');
+
 
 // Handles the opening and closing of the forms
 openModal.addEventListener("click", ()=>{
@@ -16,6 +30,21 @@ closeModal.addEventListener("click", ()=>{
 });
 
 submitBtn.addEventListener("click", ()=>{
-    console.log("Submitted");
+   
+   
+    const userInfo = {
+        "Username": username.value,
+        "Email": email.value,
+        "Phone Number": usrNumber.value,
+        "Address": address.value,
+        "Zip Code": zipCode.value,
+        "City": city.value,
+        "State": state.value,
+        "Gender": gender.value,
+        "Age": age.value
+
+    }
+
+    console.log(userInfo);
     modal.close();
 });
